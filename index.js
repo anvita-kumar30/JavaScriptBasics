f1550700e5c287cb91bb451557a7d5afd3fbf1f3
Anvita <kumaranvita30@gmail.com>
2023-06-19 13:58:52 +0530

    init

---
+++ index.js
@@ -0,0 +1,60 @@
+// 1. Ways to print in JS
+//console.log("Hello World");
+//alert("me");
+//document.write("This is document write");
+
+// 2. JavaScript console API
+//console.log("Hello World", 4 + 6, "Another log");
+//console.warn("This is a warning");
+//console.error("This is an error");
+//console.clear();
+//console.assert(4==6);  //Assertion failed
+
+// 3. JS Variables
+//Containers to store data values
+
+/*
+multi
+line
+comment
+*/
+
+var number1 = 34;
+var number2 = 56;
+console.log(number1 + number2);
+
+// 4. Data types in JS
+//Numbers
+var num1 = 455;
+var num2 = 56.6;
+//String
+var str1 = "This is a string";
+var str2 = 'This is a string';
+//Objects
+var marks = {
+    ravi: 34,
+    shubham: 78,
+    harry: 99.977
+}
+
+//Booleans
+var a = true;
+var b = false;
+
+//var und = undefined;
+var und;
+
+var n = null;
+
+/*At a very high leve;, there are two types of datatypes in JS
+1. Proimitive data types:
+2. Reference data types:
+*/
+console.log(num1);
+console.log(num2);
+console.log(str1);
+console.log(str2);
+console.log(marks);
+console.log(a,b);
+console.log(und);
+console.log(n);
